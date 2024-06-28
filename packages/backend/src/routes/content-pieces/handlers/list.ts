b086cbde757fbf886f9c84af681305323ae1ad0c
Arek Nawo <areknawo@areknawo.com>
2024-06-28 14:10:21 +0200

    feat: Get content via content-pieces/list

--- packages/backend/src/routes/content-pieces/handlers/list.ts
+++ packages/backend/src/routes/content-pieces/handlers/list.ts
@@ -6,7 +6,9 @@ import {
   getContentPieceVariantsCollection,
   contentPieceMember,
   tag,
-  contentPiece
+  contentPiece,
+  getContentsCollection,
+  getContentVariantsCollection
 } from "#collections";
 import { AuthenticatedContext } from "#lib/middleware";
 import { zodId } from "#lib/mongo";
@@ -16,8 +18,10 @@ import {
   getCanonicalLinkFromPattern,
   stringToRegex
 } from "#lib/utils";
+import { DocJSON, bufferToJSON } from "#lib/content-processing";
 
 const inputSchema = z.object({
+  content: z.boolean().describe("Whether to fetch the JSON content").default(false),
   contentGroupId: zodId()
     .describe("ID of the content group which contains the content pieces")
     .optional(),
@@ -42,10 +46,12 @@ const inputSchema = z.object({
 });
 const outputSchema = z.array(
   contentPiece.omit({ tags: true }).extend({
-    slug: z.string(),
-    tags: z.array(tag),
+    slug: z.string().describe("Slug of the content piece"),
+    tags: z.array(tag).describe("Tags assigned to the content piece"),
+    coverWidth: z.string().describe("Width of the cover image").optional(),
     members: z.array(contentPieceMember),
-    order: z.string()
+    order: z.string(),
+    content: z.record(z.string(), z.any()).describe("JSON content of the piece").optional()
   })
 );
 const handler = async (
@@ -98,10 +104,51 @@ const handler = async (
     });
   }
 
+  const contents = new Map<string, DocJSON>();
+
+  if (input.content) {
+    const contentsCollection = getContentsCollection(ctx.db);
+    const contentVariantsCollection = getContentVariantsCollection(ctx.db);
+    const contentPieceIds = contentPieces.map((contentPiece) => `${contentPiece._id}`);
+
+    if (variantId) {
+      const contentVariants = await contentVariantsCollection
+        .find({
+          contentPieceId: { $in: contentPieceIds.map((id) => new ObjectId(id)) },
+          variantId
+        })
+        .toArray();
+
+      contentVariants.forEach(({ contentPieceId, content }) => {
+        if (content) {
+          contents.set(`${contentPieceId}`, bufferToJSON(Buffer.from(content.buffer)));
+          contentPieceIds.splice(contentPieceIds.indexOf(`${contentPieceId}`), 1);
+        }
+      });
+    }
+
+    if (contentPieceIds.length > 0) {
+      const retrievedContents = await contentsCollection
+        .find({
+          contentPieceId: { $in: contentPieceIds.map((id) => new ObjectId(id)) }
+        })
+        .toArray();
+
+      retrievedContents.forEach(({ contentPieceId, content }) => {
+        if (content) {
+          contents.set(`${contentPieceId}`, bufferToJSON(Buffer.from(content.buffer)));
+        } else {
+          contents.set(`${contentPieceId}`, { type: "doc", content: [] });
+        }
+      });
+    }
+  }
+
   return Promise.all(
     contentPieces.map(async (contentPiece) => {
       const tags = await fetchContentPieceTags(ctx.db, contentPiece);
       const members = await fetchEntryMembers(ctx.db, contentPiece);
+      const content = contents.get(`${contentPiece._id}`);
 
       return {
         ...contentPiece,
@@ -116,7 +163,8 @@ const handler = async (
         workspaceId: `${contentPiece.workspaceId}`,
         date: contentPiece.date?.toISOString(),
         tags,
-        members
+        members,
+        ...(input.content && content ? { content } : {})
       };
     })
   );
